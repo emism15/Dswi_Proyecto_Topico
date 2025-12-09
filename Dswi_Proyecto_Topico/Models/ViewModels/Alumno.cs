ea8571a0a246f745539da10ca9bdb03626b7d750
LuzD2006 <mia1310177@gmail.com>
2025-12-09 01:14:16 -0500

    se agrego alumno.cs

---
+++ Dswi_Proyecto_Topico/Models/ViewModels/Alumno.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dswi_Proyecto_Topico.Models.ViewModels
+{
+    public class Alumno
+    {
+        public int AlumnoId { get; set; }
+
+        [Required(ErrorMessage = "El código es obligatorio")]
+        [RegularExpression(@"^i\d{9}$", ErrorMessage = "El código debe iniciar con 'i' seguido de 8 números")]
+        public string Codigo { get; set; }
+
+        [Required(ErrorMessage = "El nombre completo es obligatorio")]
+        public string NombreCompleto { get; set; }
+
+        [Required(ErrorMessage = "La fecha de nacimiento es obligatoria")]
+        [DataType(DataType.Date)]
+        public DateTime FechaNacimiento { get; set; }
+
+        [Required(ErrorMessage = "La edad es obligatoria")]
+        public int Edad { get; set; }
+
+        [Required(ErrorMessage = "El DNI es obligatorio")]
+        public string DNI { get; set; }
+
+        public string Telefono { get; set; }
+        [EmailAddress(ErrorMessage = "Correo inválido")]
+        public string Correo { get; set; }
+    }
+}
